d993d495003c52d0c86661081d825b33b8f72f43
Delina Weldu Kahsai <delinaweldu2808@gmail.com>
2024-07-13 15:50:47 +0300

    modified content

--- quiz.js
+++ quiz.js
@@ -13,15 +13,12 @@ const feedback = document.getElementById('feedback');
 // Check if an answer was selected
 if (userAnswer) {
     // Compare user's answer with the correct answer
-    if (userAnswer.value = correctAnswer) {
+    if (userAnswer === correctAnswer) {
         feedback.textContent = "Correct! Well Done.";
     } else {
-        feedback.textContent = "That's incorrect. Try again";
+        feedback.textContent = "That's incorrect. Try again!";
     }
-} else {
-    feedback.textContent = "please select an answer.";
 }
-
 }
 
 // Submit button
